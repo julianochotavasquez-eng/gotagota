2239b00b748fe12f7ea495c7cc6b653c240afe25
julianochotavasquez-eng <julianochotavasquez@gmail.com>
2026-04-09 16:59:39 -0500

    xd

---
+++ frontend/js/main.js
@@ -0,0 +1,11 @@
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,.)
+document.addEventListener("DOMContentLoaded", () => {
+   fetch('http://localhost:8080/api/clientes')
+       .then(response => response.json())
+       .then(data => {
+        console.log('data:', data);
+        const elemento = document.getElementById("tabla-cliente")
+        elemento
+        console.log(elemento)
+       })
+});
