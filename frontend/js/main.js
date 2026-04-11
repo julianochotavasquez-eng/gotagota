73d997d791efd8675b1647bb8f41afe415c027b8
julianochotavasquez-eng <julianochotavasquez@gmail.com>
2026-04-11 13:26:28 -0500

    aaaaa

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,35 @@
-// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,.)
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
 document.addEventListener("DOMContentLoaded", () => {
-   fetch('http://localhost:8080/api/clientes')
-       .then(response => response.json())
-       .then(data => {
-        console.log('data:', data);
-        const elemento = document.getElementById("tabla-cliente")
-        elemento
-        console.log(elemento)
-       })
+    fetch('http://localhost:8080/api/clientes')
+        .then(response => response.json())
+        .then(data => {
+            //DOM -> <tbody id= "table-cliente">
+            const elemento = document.getElementById("table-cliente")
+            for (let i = 0; i < 5; i++) {
+                //data[i], muestra en forma de array
+            let cliente = data[i]
+            // alt + 96
+            let fila = `
+                         <tr>
+                         <td>${cliente.id}</td>
+                         <td>${cliente.nombre}</td>
+                         <td>${cliente.apellido}</td>
+                         <td>${cliente.dni}</td>
+                         <td>${cliente.telefono}</td>
+                         <td>${cliente.direccion}</td>
+                         <td>
+                         <button class="btn btn-primary me-2">
+                            <i class="fas fa-edit"></i> Editar
+                        </button>
+                        <button class="btn btn-danger">
+                            <i class="fas fa-trash"></i> Eliminar0 
+                        </button>
+                   </td>
+                   </tr>
+                         `
+            elemento.innerHTML += fila
+
+            //console.log(cliente) //Muestro los resultados en consola
+            }
+        })
 });
